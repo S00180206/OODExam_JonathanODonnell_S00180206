8e918f32ca386f6d240d125cfb7bdfe9a548835f
jondonnell <S00180206@mail.itsligo.ie>
2020-05-13 10:34:47 +0100

    Properties_Q2

---
+++ OODExam_JonathanODonnell_S00180206/Phone.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace OODExam_JonathanODonnell_S00180206
+{
+    public class Phone
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string OperatingSystem { get; set; }
+        public string OS_Image { get; set; }
+        public string Phone_Image { get; set; }
+
+    }
+}
